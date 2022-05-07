7c7de10e97e78369101991b6cc529b1414f61e0a
Madhan-sai-au46 <104922258+Madhan-sai-au46@users.noreply.github.com>
2022-05-07 10:41:30 +0530

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,4 +1,13 @@
-m = input( "a =" )
-n = input( "b =" )
-print ("THE SUM OF A & B IS :",  m + n)
-print ("that is the answer")
+m = input("ENTER A NUMBER = M : ")
+n = input("ENTER A NUMBER = N : ")
+o = input("ENTER A NUMBER = 0 : ")
+p = input("ENTER A NUMBER = P : ")
+
+if (m>n and m>o and m>p):
+    print(" M IS GREATER THAN N,O,P")
+elif (n>m and n>o and n>p):
+    print("N IS GREATER THAN M,O,P")
+elif (o>m and o>n and o>p):
+    print("O IS GREATER THAN M,N,P")
+else:
+    print("P IS GREATER THAN M,N,O")
